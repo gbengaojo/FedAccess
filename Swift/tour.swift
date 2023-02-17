ebeb1c32611544ed898cc6810f06aec376a34235
Gbenga Ojo <gbenga@gbengaojo.com>
2023-02-16 18:41:17 -0600

    string interpolation, dictionaries, arrays, heredocs

--- Swift/tour.swift
+++ Swift/tour.swift
@@ -16,3 +16,17 @@ let label = "The width is "
 let width = 94
 let widthLabel = label + String(width)
 
+// ...
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+// heredoc quotation
+let quotation = """
+I said "I have \(apples) apples."
+And then I said "I have \(apples + oranges) pieces of fruit."
+"""
+
+// arrays and dictionaries
+var fruits = ["strawberries"]
