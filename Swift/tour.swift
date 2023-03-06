b08320884f34960cef4746deb0dc18e96aa3ae19
Gbenga Ojo <gbenga@lucidmediaconcepts.com>
2023-03-06 09:27:51 -0600

    `for-in` construct

--- Swift/tour.swift
+++ Swift/tour.swift
@@ -94,3 +94,21 @@ switch vegetetable {
     default:
         print("Everything tastes good in a soup.")
 }
+
+// Dictionaries are unordered collections; their keys and values are iterated
+// arbitrarily
+// `for-in`
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2,3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+for (_, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print(largest)
